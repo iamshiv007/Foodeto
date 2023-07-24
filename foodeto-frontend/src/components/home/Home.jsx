068b65c8e42d8c7bdcb05df5fb9dde6e710ec8bb
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-24 15:01:58 +0530

    F - Home, Header updated

--- foodeto-frontend/src/components/home/Home.jsx
+++ foodeto-frontend/src/components/home/Home.jsx
@@ -3,18 +3,44 @@ import { Box } from "@chakra-ui/react";
 import MetaData from "../layout/metaData/MetaData";
 import Header from "../layout/header/Header";
 import home from "../../foodeto images/cakes/swapnil-dwivedi-Nl8Oa6ZuNcA-unsplash.jpg";
+import Cake2 from "../../foodeto images/cakes/kaouther-djouada-hcEDfkiVmMI-unsplash.jpg";
 
 const Home = () => {
   return (
     <Fragment>
       <MetaData title="Foodeto" />
-      <Box>
-        <Box backgroundSize={'cover'} backgroundRepeat={'none'} backgroundImage={`url(${home})`} height={"100vh"} width={"100vw"}>
+      <Box display={{ base: "none", md: "block" }}>
+        <Box
+          backgroundSize={"cover"}
+          backgroundRepeat={"none"}
+          backgroundImage={`url(${home})`}
+          height={"100vh"}
+          width={"100vw"}
+        >
           <Header />
         </Box>
       </Box>
+      <MobileHome />
     </Fragment>
   );
 };
 
 export default Home;
+
+const MobileHome = () => {
+  return (
+    <>
+      <Box display={{ base: "block", md: "none" }}>
+        <Box
+          backgroundSize={"cover"}
+          backgroundRepeat={"none"}
+          backgroundImage={`url(${Cake2})`}
+          height={"100vh"}
+          width={"100vw"}
+        >
+          <Header />
+        </Box>
+      </Box>
+    </>
+  );
+};
--- foodeto-frontend/src/components/layout/header/Header.jsx
+++ foodeto-frontend/src/components/layout/header/Header.jsx
@@ -1,9 +1,23 @@
-import { Box, Text, Toast } from "@chakra-ui/react";
+import {
+  Box,
+  Text,
+  useDisclosure,
+  Button,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerCloseButton,
+  DrawerHeader,
+  DrawerBody,
+  IconButton,
+} from "@chakra-ui/react";
 import React, { Fragment } from "react";
 import { FaUserCircle, FaShoppingCart } from "react-icons/fa";
 import { useDispatch, useSelector } from "react-redux";
 import { NavLink, useNavigate } from "react-router-dom";
 import { logout } from "../../../featured/actions/userActions";
+import { toast } from "react-toastify";
+import { FiMenu, FiLogOut } from "react-icons/fi";
 
 const Header = () => {
   const { isAuthenticated } = useSelector((state) => state.auth);
@@ -14,13 +28,13 @@ const Header = () => {
   const logoutHandler = () => {
     dispatch(logout());
     navigate("/login");
-    Toast.success("Logout Successfully");
+    toast.success("Logout Successfully");
   };
 
   return (
     <Fragment>
       <Box
-        display={"flex"}
+        display={{ base: "none", md: "flex" }}
         justifyContent={"space-between"}
         padding={"10px 30px"}
         background={"rgba(0, 0, 0, 0.4)"}
@@ -39,7 +53,7 @@ const Header = () => {
         </Box>
 
         <Box gap={3} display={"flex"}>
-          <a href="/">
+          <NavLink to="/">
             <Box
               _hover={{ background: "#2e2e2e;" }}
               borderRadius={"10px"}
@@ -51,7 +65,8 @@ const Header = () => {
             >
               <FaShoppingCart /> Cart
             </Box>
-          </a>
+          </NavLink>
+
           <NavLink to="/profile">
             <Box
               _hover={{ background: "#2e2e2e;" }}
@@ -65,6 +80,7 @@ const Header = () => {
               <FaUserCircle /> Profile
             </Box>
           </NavLink>
+
           {isAuthenticated ? (
             <Box
               _hover={{ background: "#2e2e2e;" }}
@@ -77,7 +93,7 @@ const Header = () => {
               cursor={"pointer"}
               onClick={logoutHandler}
             >
-              <FaUserCircle /> Logout
+              <FiLogOut /> Logout
             </Box>
           ) : (
             <NavLink to="/login">
@@ -96,8 +112,118 @@ const Header = () => {
           )}
         </Box>
       </Box>
+      <MobileNavbar logoutHandler={logoutHandler} />
     </Fragment>
   );
 };
 
 export default Header;
+
+const MobileNavbar = ({ logoutHandler }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const btnRef = React.useRef();
+
+  const { isAuthenticated } = useSelector((state) => state.auth);
+
+  return (
+    <>
+      <Box padding={"20px"} display={"flex"} justifyContent={"flex-end"}>
+        <IconButton
+          display={{ base: "flex", md: "none" }}
+          ref={btnRef}
+          onClick={onOpen}
+          variant="outline"
+          color={"black"}
+          aria-label="Call Sage"
+          fontSize="20px"
+          icon={<FiMenu />}
+        />
+      </Box>
+      <Drawer
+        size="xs"
+        isOpen={isOpen}
+        placement="left"
+        onClose={onClose}
+        finalFocusRef={btnRef}
+        display={{ base: "block", sm: "none" }}
+      >
+        <DrawerOverlay />
+        <DrawerContent>
+          <DrawerCloseButton />
+          <DrawerHeader>
+            <NavLink to="/">
+              <Box>
+                <Text
+                  fontSize={"xl"}
+                  fontStyle={"italic"}
+                  fontWeight={"bold"}
+                  color={"tomato"}
+                  padding={"4px 10px"}
+                >
+                  Foodeto
+                </Text>
+              </Box>
+            </NavLink>
+          </DrawerHeader>
+
+          <DrawerBody>
+            <Box display={"flex"} flexDirection={"column"} gap={3}>
+              <NavLink to="/">
+                <Box
+                  padding={"4px 10px"}
+                  display={"flex"}
+                  gap={2}
+                  alignItems={"center"}
+                >
+                  <FaShoppingCart /> Cart
+                </Box>
+              </NavLink>
+
+              <NavLink to="/profile">
+                <Box
+                  padding={"4px 10px"}
+                  display={"flex"}
+                  gap={2}
+                  alignItems={"center"}
+                >
+                  <FaUserCircle /> Profile
+                </Box>
+              </NavLink>
+
+              {isAuthenticated ? (
+                <Box
+                  padding={"4px 10px"}
+                  display={"flex"}
+                  gap={2}
+                  alignItems={"center"}
+                  cursor={"pointer"}
+                  onClick={logoutHandler}
+                >
+                  <FiLogOut /> Logout
+                </Box>
+              ) : (
+                <NavLink to="/login">
+                  <Box
+                    padding={"4px 10px"}
+                    display={"flex"}
+                    gap={2}
+                    alignItems={"center"}
+                  >
+                    <FaUserCircle /> Login
+                  </Box>
+                </NavLink>
+              )}
+            </Box>
+          </DrawerBody>
+
+          {/* <DrawerFooter>
+            <Button variant="outline" mr={3} onClick={onClose}>
+              Cancel
+            </Button>
+            <Button colorScheme="blue">Save</Button>
+          </DrawerFooter> */}
+        </DrawerContent>
+      </Drawer>
+    </>
+  );
+};
