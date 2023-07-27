ab65d87948293929bfedb1bf86a96d4de982bc5d
iamshiv007 <iamshiv20032003@gmail.com>
2023-07-27 13:09:16 +0530

    backend -  Partner completed

--- foodeto-backend/controllers/partnerController.js
+++ foodeto-backend/controllers/partnerController.js
@@ -69,7 +69,7 @@ exports.loginPartner = catchAsyncError(async (req, res, next) => {
     sendTokenPartner(partner, 200, res)
 })
 
-// 3 Logout User
+// 3 Logout Partner
 exports.logoutPartner = catchAsyncError(async (req, res, next) => {
     res.cookie("token", null, {
         expires: new Date(Date.now()),
@@ -82,9 +82,165 @@ exports.logoutPartner = catchAsyncError(async (req, res, next) => {
     })
 })
 
-// 4 Get Partner detail
+// 4 Forgot Password
+exports.forgotPassword = catchAsyncError(async (req, res, next) => {
+    const partner = await Partner.findOne({ email: req.body.email })
+
+    if (!partner) {
+        return next(new ErrorHandler("Partner Not found", 404))
+    }
+
+
+    // Get reset password token
+    const resetToken = partner.getResetPasswordToken()
+
+    await partner.save({ validateBeforeSave: false })
+
+    const resetPasswordUrl = `${process.env.FRONTEND_URL}/password/reset/${resetToken}`
+
+    const message = `Your password reset token is :- \n\n${resetPasswordUrl} \n\nIf you have not requested this email then, please ignore it.`
+
+    try {
+        sendEmail({
+            email: partner.email,
+            subject: "Ecommerce Password Recovery",
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email  sent to ${partner.email} successfully`
+        })
+    } catch (error) {
+        partner.resetPasswordToken = undefined
+        partner.resetPasswordExpire = undefined
+
+        await partner.save({ validateBeforeSave: false })
+
+        return next(new ErrorHandler(error.message, 500))
+    }
+})
+
+// 5 Reset password
+exports.resetPassword = catchAsyncError(async (req, res, next) => {
+    // creating token hash
+    const resetPasswordToken = crypto
+        .createHash('sha256')
+        .update(req.params.token)
+        .digest("hex")
+
+    const partner = await Partner.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!partner) {
+        return next(
+            new ErrorHandler(
+                "Reset passwor token is invalid or has been expired",
+                400
+            ))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not match", 400))
+    }
+
+    partner.password = req.body.password
+    partner.resetPasswordToken = undefined
+    partner.resetTokenExpire = undefined
+
+    await Partner.save()
+
+    sendToken(partner, 200, res)
+})
+
+
+// 6. Get Partner detail
 exports.getPartnerDetails = catchAsyncError(async (req, res, next) => {
     const partner = await Partner.findById(req.partner.id)
+
+    res.status(200).json({
+        success: true,
+        partner
+    })
+})
+
+// 7 Update partner password
+exports.updatePassword = catchAsyncError(async (req, res, next) => {
+    const partner = await Partner.findById(req.partner.id).select("+password")
+
+    const isPasswordMatch = await partner.comparePassword(req.body.oldPassword)
+
+    if (!isPasswordMatch) {
+        return next(new ErrorHandler("Password does not match", 400))
+    }
+
+    if (req.body.newPassword !== req.body.confirmPassword) {
+        return next(new ErrorHandler("Password does not match", 400))
+    }
+
+    partner.password = req.body.newPassword
+
+    await partner.save()
+
+    sendToken(partner, 200, res)
+})
+
+// 8 Update Partner Profile
+exports.updateProfile = catchAsyncError(async (req, res, next) => {
+    const newPartnerData = req.body
+
+    if (req.body.shopImage !== "") {
+        const partner = await Partner.findById(req.partner.id)
+
+        const imageId = partner.avatar.public_id
+
+        await cloudinary.v2.uploader.destroy(imageId)
+
+        const myCloud = await cloudinary.v2.uploader.upload(req.body.shopImage, {
+            folder: "Shops",
+            width: 300,
+            crop: "scale"
+        })
+
+        newPartnerData.shopImage = {
+            public_id: myCloud.public_id,
+            url: myCloud.secure_url
+        }
+    }
+
+    const partner = await Partner.findByIdAndUpdate(req.partner.id, newPartnerData, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false
+    })
+
+    res.status(200).json({
+        success: true,
+        partner
+    })
+})
+
+// 9 Get all Partners -- admin
+exports.getAllPartners = catchAsyncError(async (req, res, next) => {
+    const partners = await Partner.find()
+
+    res.status(200).json({
+        success: true,
+        partners
+    })
+})
+
+// 10 Get single partner -- admin
+exports.getSinglePartner = catchAsyncError(async (req, res, next) => {
+    const partner = await Partner.findById(req.params.id)
+
+    if (!partner) {
+        return next(
+            new ErrorHandler(`Partner is not axist with this Partner id : ${req.params.id}}`)
+        )
+    }
 
     res.status(200).json({
         success: true,
